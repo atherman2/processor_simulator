1cc4bcec41b236e9e7269a932ff9804e9ee19acd
atherman2 <alexandre.t.herman@gmail.com>
2024-10-04 10:17:54 -0300

    valores -> ponteiros (build_cache())

--- files/typedefs.c
+++ files/typedefs.c
@@ -7,12 +7,12 @@ typedef struct Cache_line
 
 typedef struct Cache_set
 {
-    Cache_line* lines;
+    Cache_line** lines;
 } Cache_set;
 
 typedef struct Cache
 {
-    Cache_set* sets;
+    Cache_set** sets;
     int substitution_index;
     int set_quantity;
 } Cache;
