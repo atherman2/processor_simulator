593759f3bd1acebf54e941e938f433f7c6cc4c6d
atherman2 <alexandre.t.herman@gmail.com>
2024-10-04 10:30:50 -0300

    build_cache() separada em mais funções

--- files/cache.c
+++ files/cache.c
@@ -3,8 +3,6 @@
 
 void build_cache(Cache* cache, int words_per_line, int lines_per_set, int sets_per_cache)
 {
-    int words_remaining;
-    int lines_remaining;
     int sets_remaining;
 
     cache->sets = (Cache_set*) malloc(sets_per_cache * sizeof(Cache_set));
@@ -12,21 +10,33 @@ void build_cache(Cache* cache, int words_per_line, int lines_per_set, int sets_p
     for(sets_remaining = sets_per_cache - 1; sets_remaining >= 0; sets_remaining--)
     {
         Cache_set* set;
-        set->lines = (Cache_line*) malloc(lines_per_set * sizeof(Cache_line));
-
-        for(lines_remaining = lines_per_set - 1; lines_remaining >= 0; lines_remaining--)
-        {
-            Cache_line* line;
-            line->words = (Word*) malloc(words_per_line * sizeof(Word));
-
-            for(words_remaining = words_per_line - 1; words_remaining >= 0; words_remaining--)
-            {
-                line->words[words_remaining] = 0;
-            }
-            set->lines[lines_remaining] = line;
-        }
+        build_set(set, lines_per_set, words_per_line);
         cache->sets[sets_remaining] = set;
     }
 }
 
+void build_set(Cache_set* set, int lines_per_set, int words_per_line)
+{
+    int lines_remaining;
+    
+    set->lines = (Cache_line*) malloc(lines_per_set * sizeof(Cache_line));
 
+    for(lines_remaining = lines_per_set - 1; lines_remaining >= 0; lines_remaining--)
+    {
+        Cache_line* line;
+        build_line(line, words_per_line);
+        set->lines[lines_remaining] = line;
+    }
+}
+
+void build_line(Cache_line* line, int words_per_line)
+{
+    int words_remaining;
+    
+    line->words = (Word*) malloc(words_per_line * sizeof(Word));
+
+    for(words_remaining = words_per_line - 1; words_remaining >= 0; words_remaining--)
+    {
+        line->words[words_remaining] = 0;
+    }
+}
